cd8425e1560e74f7dffa9cc0ece4ce0d44082464
MuzaFir <88869100+muzxfir@users.noreply.github.com>
2026-08-05 05:41:00 +0400

    Add files via upload

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -11,46 +11,43 @@ import "leaflet/dist/leaflet.css";
 
 const markerIcon = L.divIcon({
   className: "custom-country-marker",
-  html: '<span>●</span>',
-  iconSize: [28, 28],
-  iconAnchor: [14, 14],
-  popupAnchor: [0, -12],
+  html: "<span></span>",
+  iconSize: [26, 26],
+  iconAnchor: [13, 13],
+  popupAnchor: [0, -13],
 });
 
-function MapController({ country }) {
+function ChangeView({ country }) {
   const map = useMap();
 
   useEffect(() => {
     if (country?.latlng?.length === 2) {
-      map.flyTo(country.latlng, 5, {
-        duration: 1.3,
-      });
+      map.flyTo(country.latlng, 5, { duration: 1.2 });
+    } else {
+      map.flyTo([20, 0], 2, { duration: 1 });
     }
   }, [country, map]);
 
   return null;
 }
 
-export default function MapView({ country }) {
-  const position =
-    country?.latlng?.length === 2 ? country.latlng : [20, 0];
-
+export default function MapView({ country, large = false }) {
   return (
-    <div className="leaflet-map-card">
+    <div className={large ? "leaflet-map-card large" : "leaflet-map-card"}>
       <MapContainer
-        center={position}
-        zoom={country ? 5 : 2}
+        center={[20, 0]}
+        zoom={2}
         minZoom={2}
         maxZoom={18}
         scrollWheelZoom
         className="leaflet-map"
       >
         <TileLayer
-          attribution='&copy; OpenStreetMap contributors'
+          attribution="&copy; OpenStreetMap contributors"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-        <MapController country={country} />
+        <ChangeView country={country} />
 
         {country?.latlng?.length === 2 && (
           <Marker position={country.latlng} icon={markerIcon}>
